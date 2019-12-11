860b5c86d404d911158916e5c6acf8226dfc97b1
Nim <Nim@localhost.localdomain>
2019-12-11 09:57:52 +0100

    stop the shell AFTER cleaning up you fking moron

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-systemctl stop prometheus
 systemctl disable prometheus
 rm -rf /etc/prometheus
 rm /lib/systemd/system/prometheus.service
 systemctl daemon-reload
+systemctl stop prometheus
